97a8965c600a8f60eaf37e2fbb45ade532e0a038
valnor-global <rodriii93@gmail.com>
2025-05-13 22:41:27 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '2rem' }}>
+      <h1>Valnör Global LLC</h1>
+      <p>Connecting manufacturers with reliable buyers across Latin America.</p>
+    </div>
+  );
+}
